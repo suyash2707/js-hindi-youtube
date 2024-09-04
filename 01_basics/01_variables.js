178b1bf285f709a9f740ad706af28f241129a950
suyash2707 <suyashdwivedi27.07@gmail.com>
2024-09-04 20:05:29 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=123456
+let accountEmail="suyashdwivedi@gmail.com"
+var accountPassword="12345667"
+accountCity="Lucknow"
+
+let accountState;
+//accountId=2  not allowed to change
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
+//JS uses only LET and CONST we should avoid using VAR because of issue in block and functional scope
